614b95a1ca8a1cb5e3beba39663ecec57b4a9b07
송지한 <157107006+Song-Jihan@users.noreply.github.com>
2026-07-12 18:34:48 +0900

    feat)testing on laptop local url

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 window.GEO_CONFIG = {
-  API_BASE_URL: "https://desktop-home.taile10591.ts.net"
+  //https://desktop-home.taile10591.ts.net // 배포용
+  API_BASE_URL: "https://ssonglaptop.taile10591.ts.net"
 };
