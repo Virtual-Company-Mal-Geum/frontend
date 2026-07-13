f7d2e253fb0bfaf603ee0d419f52539cf60f9b78
송지한 <157107006+Song-Jihan@users.noreply.github.com>
2026-07-13 16:32:16 +0900

    feat)api for desktop

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GEO_CONFIG = {
-  //https://desktop-home.taile10591.ts.net // 배포용
-  API_BASE_URL: "https://ssonglaptop.taile10591.ts.net"
+  API_BASE_URL: "https://desktop-home.taile10591.ts.net"
+  //API_BASE_URL: "https://ssonglaptop.taile10591.ts.net"
 };
